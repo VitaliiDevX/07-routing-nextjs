d1f038d0978c2c7e04b7594a94849abe2b86c48b
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-01-12 16:01:20 +0200

    fixed model

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,9 +1,28 @@
-import NotePreviewClient from "./NotePreview.client";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
 
-export default function NoteModal() {
+interface NoteModalProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function NoteModal({ params }: NoteModalProps) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["fetchNoteById", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
   return (
-    <>
-      <NotePreviewClient />
-    </>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
   );
 }
